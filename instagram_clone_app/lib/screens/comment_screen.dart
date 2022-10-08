09fb3dbf6d1fe7853f12ed3a19b1939abd01189f
Animesh Shukla <animeshshukla1518@gmail.com>
2022-10-08 18:57:19 +0530

    added bottom navigation bar in comment screen

--- instagram_clone_app/lib/screens/comment_screen.dart
+++ instagram_clone_app/lib/screens/comment_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone_app/utils/colors.dart';
 
+import '../widgets/comment_card.dart';
+
 class CommentScren extends StatefulWidget {
   const CommentScren({Key? key}) : super(key: key);
 
@@ -16,6 +18,55 @@ class _CommentScrenState extends State<CommentScren> {
         backgroundColor: mobileBackgroundColor,
         title: const Text('Comments'),
         centerTitle: false,
+      ),
+      body: CommentCard(),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: kToolbarHeight,
+          margin: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          padding: const EdgeInsets.only(
+            right: 8,
+            left: 16,
+          ),
+          child: Row(
+            children: [
+              const CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://images.unsplash.com/photo-1665179489306-a5158530b8ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+                ),
+                radius: 18,
+              ),
+              const Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 16.0, right: 8),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Comment as Username',
+                        border: InputBorder.none),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 8,
+                  ),
+                  child: const Text(
+                    'Post',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ instagram_clone_app/lib/widgets/comment_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CommentCard extends StatefulWidget {
+  const CommentCard({Key? key}) : super(key: key);
+
+  @override
+  State<CommentCard> createState() => _CommentCardState();
+}
+
+class _CommentCardState extends State<CommentCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
