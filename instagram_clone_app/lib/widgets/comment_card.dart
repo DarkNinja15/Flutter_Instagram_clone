570eafcaaf598f6e531b70ab699d9709f29d79b1
Animesh Shukla <animeshshukla1518@gmail.com>
2022-10-08 19:08:00 +0530

    added dummy comment card screen

--- instagram_clone_app/lib/widgets/comment_card.dart
+++ instagram_clone_app/lib/widgets/comment_card.dart
@@ -10,6 +10,70 @@ class CommentCard extends StatefulWidget {
 class _CommentCardState extends State<CommentCard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 18,
+        horizontal: 16,
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://images.unsplash.com/photo-1665153651313-2dd0959fc4ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+            ),
+            radius: 18,
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: 16,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Username',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Some Description to Enter',
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 4,
+                    ),
+                    child: Text(
+                      '23/12/2021',
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.favorite_border_outlined,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
