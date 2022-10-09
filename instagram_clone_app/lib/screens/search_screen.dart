addec3d23b8b1b9ccc9fbecb25fa71fdbeb683ac
Animesh Shukla <animeshshukla1518@gmail.com>
2022-10-09 11:00:20 +0530

    added grid view

--- instagram_clone_app/lib/screens/search_screen.dart
+++ instagram_clone_app/lib/screens/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram_clone_app/utils/colors.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -70,7 +71,31 @@ class _SearchScreenState extends State<SearchScreen> {
                     });
               },
             )
-          : Text('Posts'),
+          : FutureBuilder(
+              future: FirebaseFirestore.instance.collection('posts').get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return StaggeredGridView.countBuilder(
+                  crossAxisCount: 3,
+                  itemCount: (snapshot.data! as dynamic).docs.length,
+                  itemBuilder: (context, index) {
+                    return Image.network(
+                      (snapshot.data! as dynamic).docs[index]['postUrl'],
+                      fit: BoxFit.cover,
+                    );
+                  },
+                  staggeredTileBuilder: (index) => StaggeredTile.count(
+                    (index % 7 == 0) ? 2 : 1,
+                    (index % 7 == 0) ? 2 : 1,
+                  ),
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                );
+              }),
     );
   }
 }
